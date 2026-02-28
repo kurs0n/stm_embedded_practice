fb70df855880f5a24943272b3c64baabdfa30067
Patryk Kurek <patrykkurek@student.agh.edu.pl>
2026-02-28 21:55:15 +0100

    feat: sending spi message with first byte to determine length and sending list of bytes for raspberry pi pico (working transfer)

--- driver_development/drivers/Src/spi_driver.c
+++ driver_development/drivers/Src/spi_driver.c
@@ -1,99 +1,102 @@
 #include "spi_driver.h"
-
-void inline __attribute__((always_inline)) delay(uint32_t delay)
-{
-   while(delay--) __asm("");
+void inline __attribute__((always_inline)) delay(uint32_t delay) {
+  while (delay--)
+    __asm("");
 }
 
-void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi){
-    switch(EnorDi){
-        case ENABLE:{
-            if(pSPIx == SPI1){
-                SPI1_PCLK_EN();
-            } else if(pSPIx == SPI2) {
-                SPI2_PCLK_EN();
-            } else if(pSPIx == SPI3) {
-                SPI3_PCLK_EN();
-            } else if(pSPIx == SPI4) {
-                SPI4_PCLK_EN();
-            }
-            break;
-        }
-        case DISABLE:{
-            if(pSPIx == SPI1){
-                SPI1_PCLK_DI();
-            } else if(pSPIx == SPI2) {
-                SPI2_PCLK_DI();
-            } else if(pSPIx == SPI3) {
-                SPI3_PCLK_DI();
-            } else if(pSPIx == SPI4) {
-                SPI4_PCLK_DI();
-            }
-            break;
-        }
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi) {
+  switch (EnorDi) {
+  case ENABLE: {
+    if (pSPIx == SPI1) {
+      SPI1_PCLK_EN();
+    } else if (pSPIx == SPI2) {
+      SPI2_PCLK_EN();
+    } else if (pSPIx == SPI3) {
+      SPI3_PCLK_EN();
+    } else if (pSPIx == SPI4) {
+      SPI4_PCLK_EN();
     }
-}
- 
-void SPI_Init(SPI_Handle_t *pSPIHandle){
-    uint32_t control_register1 = 0;
-    uint32_t control_register2 = 0;
-    
-    pSPIHandle->pSPIx->CR1 = 0;
-    pSPIHandle->pSPIx->CR2 = 0;
-
-    pSPIHandle->pSPIx->CR1 &= ~(ENABLE << SPI_CR1_SPE);
-
-    control_register1 |= pSPIHandle->SPIConfig.SPI_DeviceMode<<SPI_CR1_MSTR;
-    
-    if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD){
-        control_register1 &= ~(ENABLE<<SPI_CR1_BIDIMODE); 
-    } else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD){
-        control_register1 |= ENABLE<<SPI_CR1_BIDIMODE;
-    } else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY){
-        control_register1 &= ~(ENABLE<<SPI_CR1_BIDIMODE); 
-        control_register1 |= ENABLE<<SPI_CR1_RXONLY;
+    break;
+  }
+  case DISABLE: {
+    if (pSPIx == SPI1) {
+      SPI1_PCLK_DI();
+    } else if (pSPIx == SPI2) {
+      SPI2_PCLK_DI();
+    } else if (pSPIx == SPI3) {
+      SPI3_PCLK_DI();
+    } else if (pSPIx == SPI4) {
+      SPI4_PCLK_DI();
     }
-    
-    control_register1 |= (pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR);
-    control_register1 |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
-    control_register1 |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
-    control_register1 |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
-    control_register1 |= pSPIHandle->SPIConfig.SPI_BIDIOE << SPI_CR1_BIDIOE;
-    control_register1 |= (pSPIHandle->SPIConfig.SPI_SSI << SPI_CR1_SSI);
-
-    control_register2 |= (pSPIHandle->SPIConfig.SPI_DFF << SPI_CR2_DS);
-    control_register2 |= (pSPIHandle->SPIConfig.SPI_SSOE << SPI_CR2_SSOE);
-    control_register2 |= (pSPIHandle->SPIConfig.SPI_FRXTH << SPI_CR2_FRXTH); 
-    
-    pSPIHandle->pSPIx->CR2 = control_register2;
-    pSPIHandle->pSPIx->CR1 = control_register1;
+    break;
+  }
+  }
 }
 
-void SPI_Enable(SPI_Handle_t *pSPIHandle, uint8_t enOrDis){
-    if(enOrDis == ENABLE)
-        pSPIHandle->pSPIx->CR1 |= (1 << SPI_CR1_SPE);
-    else
-        pSPIHandle->pSPIx->CR1 &= ~(1 << SPI_CR1_SPE);
+void SPI_Init(SPI_Handle_t *pSPIHandle) {
+  uint32_t control_register1 = 0;
+  uint32_t control_register2 = 0;
+
+  pSPIHandle->pSPIx->CR1 = 0;
+  pSPIHandle->pSPIx->CR2 = 0;
+
+  pSPIHandle->pSPIx->CR1 &= ~(ENABLE << SPI_CR1_SPE);
+
+  control_register1 |= pSPIHandle->SPIConfig.SPI_DeviceMode << SPI_CR1_MSTR;
+
+  if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD) {
+    control_register1 &= ~(ENABLE << SPI_CR1_BIDIMODE);
+  } else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD) {
+    control_register1 |= ENABLE << SPI_CR1_BIDIMODE;
+  } else if (pSPIHandle->SPIConfig.SPI_BusConfig ==
+             SPI_BUS_CONFIG_SIMPLEX_RXONLY) {
+    control_register1 &= ~(ENABLE << SPI_CR1_BIDIMODE);
+    control_register1 |= ENABLE << SPI_CR1_RXONLY;
+  }
+
+  control_register1 |= (pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR);
+  control_register1 |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
+  control_register1 |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
+  control_register1 |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
+  control_register1 |= pSPIHandle->SPIConfig.SPI_BIDIOE << SPI_CR1_BIDIOE;
+  control_register1 |= (pSPIHandle->SPIConfig.SPI_SSI << SPI_CR1_SSI);
+
+  control_register2 |= (pSPIHandle->SPIConfig.SPI_DFF << SPI_CR2_DS);
+  control_register2 |= (pSPIHandle->SPIConfig.SPI_SSOE << SPI_CR2_SSOE);
+  control_register2 |= (pSPIHandle->SPIConfig.SPI_FRXTH << SPI_CR2_FRXTH);
+
+  pSPIHandle->pSPIx->CR2 = control_register2;
+  pSPIHandle->pSPIx->CR1 = control_register1;
 }
 
-void SPI_DeInit(SPI_RegDef_t *pSPIx){
-    if(pSPIx == SPI1){
-        SPI1_REG_RESET();
-    } else if(pSPIx == SPI2){
-        SPI2_REG_RESET();
-    } else if(pSPIx == SPI3){
-        SPI3_REG_RESET();
-    } else if(pSPIx == SPI4){
-        SPI4_REG_RESET();
-    }
+void SPI_Enable(SPI_Handle_t *pSPIHandle, uint8_t enOrDis) {
+  if (enOrDis == ENABLE)
+    pSPIHandle->pSPIx->CR1 |= (1 << SPI_CR1_SPE);
+  else
+    pSPIHandle->pSPIx->CR1 &= ~(1 << SPI_CR1_SPE);
 }
 
-void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len){
-    while( Len > 0) {
-        while( ! (pSPIx->SR & (1 << SPI_SR_TXE)) );
-        *(volatile uint8_t *)&pSPIx->DR = *pTxBuffer;   // this approach work for setting DS to just 8 bits
-        Len--;
-        pTxBuffer++;
-    }
+void SPI_DeInit(SPI_RegDef_t *pSPIx) {
+  if (pSPIx == SPI1) {
+    SPI1_REG_RESET();
+  } else if (pSPIx == SPI2) {
+    SPI2_REG_RESET();
+  } else if (pSPIx == SPI3) {
+    SPI3_REG_RESET();
+  } else if (pSPIx == SPI4) {
+    SPI4_REG_RESET();
+  }
 }
- 
+
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len) {
+  while (Len > 0) {
+    while (!(pSPIx->SR & (1 << SPI_SR_TXE)))
+      ;
+    *(volatile uint8_t *)&pSPIx->DR =
+        *pTxBuffer; // this approach work for setting DS to just 8 bits
+    Len--;
+    pTxBuffer++;
+  }
+  while (pSPIx->SR & (1 << SPI_SR_BSY)) {
+  }
+}
--- spi_pico_raspberry/main.c
+++ spi_pico_raspberry/main.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/spi.h"
 #include "hardware/gpio.h"
+#include "hardware/spi.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    spi_init(spi0, 1000); // baudrate ignored in slave mode 
-    spi_set_slave(spi0, true);
-    spi_set_format(spi0, 8, SPI_CPOL_0, SPI_CPHA_1, SPI_MSB_FIRST); // Lesson learned! CPHA_1 is more stable than CPHA_0 for some reason CPHA_0 is getting only the first byte
+  spi_init(spi0, 1000); // baudrate ignored in slave mode
+  spi_set_slave(spi0, true);
+  spi_set_format(
+      spi0, 8, SPI_CPOL_0, SPI_CPHA_1,
+      SPI_MSB_FIRST); // Lesson learned! CPHA_1 is more stable than CPHA_0 for
+                      // some reason CPHA_0 is getting only the first byte
 
-    gpio_set_function(2, GPIO_FUNC_SPI); 
-    gpio_set_function(4, GPIO_FUNC_SPI); 
-    gpio_set_function(5, GPIO_FUNC_SPI); 
-   
-    uint8_t buf[12];
+  gpio_set_function(2, GPIO_FUNC_SPI);
+  gpio_set_function(4, GPIO_FUNC_SPI);
+  gpio_set_function(5, GPIO_FUNC_SPI);
 
-    while (true) {
-        spi_read_blocking(spi0, 0x00, buf, sizeof(buf));
+  uint8_t howManyBytes = 0;
+  uint8_t buf[12];
 
-        for (int i = 0; i < sizeof(buf); i++) {
-            printf("%02X ", buf[i]);
-        }
-        printf("\n");
+  while (true) {
+    spi_read_blocking(spi0, 0x00, &howManyBytes, 1);
+
+    spi_read_blocking(spi0, 0x00, buf, howManyBytes);
+    for (int i = 0; i < howManyBytes; i++) {
+      printf("%02X ", buf[i]);
     }
+    printf("\n");
+  }
 }
